1a197b5a0d0087346a4a8030b32b2770fbdf4d6f
olebol <ole.pelser@gmail.com>
2024-10-30 23:37:38 +0000

    Add LIST command to the command store

--- include/General.hpp
+++ include/General.hpp
@@ -48,6 +48,7 @@ bool PART(IRStream& stream, string& args, User* user);
 bool KICK(IRStream &stream, string &args, User *user);
 bool INVITE(IRStream &stream, string &args, User *user);
 bool MODE(IRStream& stream, string& args, User* user);
+bool LIST(IRStream& stream, string& args, User* user);
 
 /**
  * @brief The store of all the commands. This is a map of strings to function pointers. Used by the parser to determine
@@ -56,6 +57,6 @@ bool MODE(IRStream& stream, string& args, User* user);
 const map<string, bool (*)(IRStream&, string&, User*)> store = {
 	{"PASS", PASS}, {"CAP", CAP},	{"NICK", NICK},		  {"MOTD", MOTD}, {"USER", USER},  {"INFO", INFO},
 	{"JOIN", JOIN}, {"PING", PING}, {"PRIVMSG", PRIVMSG}, {"WHO", WHO},	  {"TOPIC", TOPIC}, {"PART", PART}, 
-	{"KICK", KICK}, {"INVITE", INVITE}, {"MODE", MODE}};
+	{"KICK", KICK}, {"INVITE", INVITE}, {"MODE", MODE}, {"LIST", LIST}};
 
 bool parse(User* user);
