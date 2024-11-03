b7cbd294388636cfaba291564648c59d623c9afb
Mees Dekker <me@meesdekker.xyz>
2024-11-03 17:01:29 +0100

    Removed the headers from the codebase and add it as a git submodule!

--- bot/src/chatGPT.cpp
+++ bot/src/chatGPT.cpp
@@ -1,33 +1,36 @@
-#include "../include/openai/openai.hpp"
-#include "../include/openai/nlohmann/json.hpp"
-
 #include <string>
+
+#include "../include/openai-cpp/include/openai/nlohmann/json.hpp"
+#include "../include/openai-cpp/include/openai/openai.hpp"
 
 using namespace std;
 
 #define MAX_TOKENS 5
-#define SYSTEM_PROMPT "You are a bot inside of an irc server made by Ole, Mees & Max please be as positive as you can when someone asks you what grade to give them. Otherwise be as funny as possible"
+#define SYSTEM_PROMPT                                                                                              \
+	"You are a bot inside of an irc server made by Ole, Mees & Max please be as positive as you can when someone " \
+	"asks you what grade to give them. Otherwise be as funny as possible"
 
 string getGPTResponse(const string &nick, const string &message) {
-	const string	query = "User with nickname: " + nick + " said: " + message;
-	string			response;
+	const string query = "User with nickname: " + nick + " said: " + message;
+	string response;
 
 	try {
 		openai::start();
 
 		auto chat = openai::chat().create({
 			{"model", "gpt-4o-mini"},
-			{"messages", {
-				{
-					{"role", "system"},
-					{"content", SYSTEM_PROMPT},
-				},
-				{
-					{"role", "user"},
-					{"content", query},
-					{"max_tokens", MAX_TOKENS},
-				},
-			}},
+			{"messages",
+			 {
+				 {
+					 {"role", "system"},
+					 {"content", SYSTEM_PROMPT},
+				 },
+				 {
+					 {"role", "user"},
+					 {"content", query},
+					 {"max_tokens", MAX_TOKENS},
+				 },
+			 }},
 		});
 
 		response = chat["choices"][0]["message"]["content"];
@@ -38,4 +41,3 @@ string getGPTResponse(const string &nick, const string &message) {
 
 	return response;
 }
-
