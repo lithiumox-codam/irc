ae48ecc78fd9070ce0167ba5e8d5068a4dc35f8a
meesdekker.xyz <me@meesdekker.xyz>
2024-11-28 20:09:09 +0000

    Big MODE energy

--- src/commands/MODE.cpp
+++ src/commands/MODE.cpp
@@ -13,33 +13,113 @@
 
 extern Server server;
 
-void applyModeChanges(Modes &modes, string &modeChanges) {
-	const string modeChars = "ovimktl";
-	const unsigned int modeValues[] = {M_OPERATOR, M_VOICE,		 M_INVISIBLE, M_MODERATED,
-									   M_PASSWORD, M_TOPIC_LOCK, M_LIMIT};
-	for (size_t i = 0; i < modeChanges.size(); ++i) {
-		if (modeChanges[i] == '+' || modeChanges[i] == '-') {
-			bool addMode = (modeChanges[i] == '+');
-			unsigned int modeToApply = 0;
+void handleUserMode(IRStream &stream, vector<string> &tokens, User *user) {
+	if (tokens.size() == 2 && tokens[1] != user->getNickname()) {
+		stream.prefix()
+			.code(ERR_USERSDONTMATCH)
+			.param(user->getNickname())
+			.trail("Cannot change modes for other users")
+			.end();
+		return;
+	}
 
-			if (modeChanges[i + 1] == 'i') {
-				if (modes.getType() == Type::CHANNEL) {
-					modeToApply = M_INVITE_ONLY;
-				} else if (modes.getType() == Type::USER) {
-					modeToApply = M_INVISIBLE;
-				}
-			} else {
-				modeToApply = modeValues[modeChars.find(modeChanges[i + 1])];
+	if (tokens.size() == 1) {
+		try {
+			auto *member = server.getUser(tokens[0]);
+			if (member->getNickname() != user->getNickname()) {
+				throw NoOtherUserModeException();
 			}
-
-			if (addMode) {
-				modes.addModes(modeToApply);
-			} else {
-				modes.removeModes(modeToApply);
-			}
-			++i;
+			stream.prefix()
+				.code(RPL_UMODEIS)
+				.param(user->getNickname())
+				.trail("User modes: +" + user->modes.getModesString())
+				.end();
+			return;
+		} catch (const IrcException &e) {
+			e.e_stream(stream, user);
+			return;
 		}
 	}
+	if (tokens.size() == 2) {
+		auto [changedModes, unsupportedModes] = applyModeChanges(user->modes, tokens[1]);
+		if (!unsupportedModes.empty()) {
+			stream.prefix()
+				.code(ERR_UNKNOWNMODE)
+				.trail("Unsupported modes: " + std::string(unsupportedModes.begin(), unsupportedModes.end()))
+				.end();
+		}
+		stream.prefix()
+			.code(RPL_UMODEIS)
+			.param(user->getNickname())
+			.trail("User modes: +" + user->modes.getModesString())
+			.end();
+		return;
+	}
+	stream.prefix().code(ERR_UNKNOWNMODE).trail("Unknown mode").end();
+}
+
+void handleChannelMode(IRStream &stream, vector<string> &tokens, User *user) {
+	try {
+		auto *channel = server.getChannel(tokens.front());
+		auto member = channel->getMember(user->getNickname());
+
+		if (!member.second.hasModes(M_OPERATOR)) {
+			throw UserNotOperatorException();
+		}
+
+		if (tokens.size() == 1) {
+			stream.prefix()
+				.code(RPL_CHANNELMODEIS)
+				.param(user->getNickname())
+				.param(channel->getName())
+				.trail("+" + channel->modes.getModesString())
+				.end();
+			return;
+		}
+		if (tokens.size() == 2) {
+			if (tokens[1].starts_with("+") || tokens[1].starts_with("-")) {
+				auto [changedModes, unsupportedModes] = applyModeChanges(channel->modes, tokens[1]);
+				stream.prefix()
+					.code(RPL_CHANNELMODEIS)
+					.param(user->getNickname())
+					.param(channel->getName())
+					.trail("+" + channel->modes.getModesString())
+					.end();
+				return;
+			}
+			try {
+				auto &member = channel->getMember(tokens[1]);
+				stream.prefix()
+					.code(RPL_UMODEIS)
+					.param(user->getNickname())
+					.trail(member.first->getNickname() + " User modes: +" + member.second.getModesString())
+					.end();
+			} catch (const IrcException &e) {
+				e.e_stream(stream, user);
+				return;
+			}
+		}
+		if (tokens.size() == 3) {
+			try {
+				auto &member = channel->getMember(tokens[2]);
+				auto [changedModes, unsupportedModes] = applyModeChanges(member.second, tokens[1]);
+				if (!unsupportedModes.empty()) {
+					stream.prefix()
+						.code(ERR_UNKNOWNMODE)
+						.trail("Unsupported modes: " + std::string(unsupportedModes.begin(), unsupportedModes.end()))
+						.end();
+					return;
+				}
+			} catch (const IrcException &e) {
+				e.e_stream(stream, user);
+				return;
+			}
+		}
+	} catch (const IrcException &e) {
+		e.e_stream(stream, user);
+		return;
+	}
+	stream.prefix().code(ERR_UNKNOWNMODE).trail("Unknown mode").end();
 }
 
 void MODE(IRStream &stream, string &args, User *user) {
@@ -51,54 +131,8 @@ void MODE(IRStream &stream, string &args, User *user) {
 	vector<string> tokens = split(args, ' ');
 
 	if (tokens.front().starts_with("#")) {
-		try {
-			auto *channel = server.getChannel(tokens.front());
-			if (tokens.size() == 1) {
-				stream.prefix()
-					.code(RPL_CHANNELMODEIS)
-					.param(user->getNickname())
-					.param(channel->getName())
-					.trail("+" + channel->modes.getModesString())
-					.end();
-				return;
-			}
-			if (tokens.size() == 2) {
-				if (tokens[1].starts_with("+") || tokens[1].starts_with("-")) {
-					applyModeChanges(channel->modes, tokens[1]);
-					stream.prefix()
-						.code(RPL_CHANNELMODEIS)
-						.param(user->getNickname())
-						.param(channel->getName())
-						.trail("+" + channel->modes.getModesString())
-						.end();
-					return;
-				}
-				try {
-					auto &member = channel->getMember(tokens[1]);
-					stream.prefix()
-						.code(RPL_UMODEIS)
-						.param(user->getNickname())
-						.trail(member.first->getNickname() + " User modes: +" + member.second.getModesString())
-						.end();
-				} catch (const IrcException &e) {
-					e.e_stream(stream, user);
-					return;
-				}
-			}
-			if (tokens.size() == 3) {
-				try {
-					auto &member = channel->getMember(tokens[2]);
-					applyModeChanges(member.second, tokens[1]);
-
-				} catch (const IrcException &e) {
-					e.e_stream(stream, user);
-					return;
-				}
-			}
-
-		} catch (const IrcException &e) {
-			e.e_stream(stream, user);
-			return;
-		}
+		handleChannelMode(stream, tokens, user);
+	} else {
+		handleUserMode(stream, tokens, user);
 	}
 }
