df2def4ca2ddaa134a66e190334195db22ab22df
Olebol <ole.pelser@gmail.com>
2024-12-02 22:08:12 +0000

    fix compile issue (reorder) and properly copy

--- src/IRStream.cpp
+++ src/IRStream.cpp
@@ -163,8 +163,7 @@ void IRStream::sendPacket(User *user) {
 	}
 	// NOLINTNEXTLINE
 	cout << "[\033[1;" << (31 + (user->getSocket() % 6)) << "m" << user->getNickname() << "\033[0m] "
-		 << (user->hasHandshake(H_AUTHENTICATED) ? "🔒" : "") << (user->modes.has(M_OPERATOR) ? "🧏" : "")
-		 << "\n"
+		 << (user->hasHandshake(H_AUTHENTICATED) ? "🔒" : "") << (user->modes.has(M_OPERATOR) ? "🧏" : "") << "\n"
 		 << user->getOutBuffer() << "\n";
 }
 
