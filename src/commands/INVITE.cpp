d62b03e224384c2ef390f18b159bf1fbac099f81
Max Valk <jcamvalk@gmail.com>
2024-12-02 22:56:45 +0100

    updated the invite command

--- src/commands/INVITE.cpp
+++ src/commands/INVITE.cpp
@@ -12,43 +12,29 @@
 extern Server server;
 
 void INVITE(IRStream &stream, string &args, User *user) {
-	if (!user->hasHandshake(USER_AUTHENTICATED)) {
-		stream.prefix().code(ERR_NOTREGISTERED).param(user->getNickname()).trail("You have not registered").end();
+	pair<string, string> tokens = splitPair(args, ' ');
+
+	if (tokens.first.empty() || tokens.second.empty()) {
+		stream.prefix().code(ERR_NEEDMOREPARAMS).param(user->getNickname()).trail("Not enough parameters").end();
 		return;
 	}
-
-	pair<string, string> tokens = splitPair(args, ' ');
-	string inviteeName = tokens.first;
-	string channelName = tokens.second;
-
 	try {
-		if (channelName.empty() || inviteeName.empty()) {
-			throw NotEnoughParametersException();
-		}
-
-		Channel *channel = server.getChannel(channelName);
+		Channel *channel = server.getChannel(tokens.first);
 		pair<User *, Modes> *inviter = channel->getMember(user);
-
 		if (inviter == nullptr) {
 			throw NotOnChannelException(user->getNickname());
 		}
-
 		if (channel->modes.hasModes(M_INVITE_ONLY) && !inviter->second.hasModes(M_OPERATOR)) {
 			throw UserNotOperatorException();
 		}
-
-		User *invitee = server.getUser(inviteeName);
-
+		User *invitee = server.getUser(tokens.second);
 		if (channel->getMember(invitee) != nullptr) {
 			throw UserAlreadyOnChannelException();
 		}
-
 		channel->addInvited(invitee);
-
-		stream.prefix().code(RPL_INVITING).param(user->getNickname()).param(inviteeName).param(channelName).end();
+		stream.prefix().code(RPL_INVITING).param(user->getNickname()).param(tokens.second).param(tokens.first).end();
 		IRStream invStream;
-		invStream.prefix(user).param("INVITE").param(inviteeName).param(channelName).end().sendPacket(invitee);
-
+		invStream.prefix(user).param("INVITE").param(tokens.second).param(tokens.first).end().sendPacket(invitee);
 	} catch (const IrcException &e) {
 		e.e_stream(stream, user);
 	}
