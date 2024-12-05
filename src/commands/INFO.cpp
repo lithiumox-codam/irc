fae3df543ae0169f230410593993ce78958df2dc
olebol <ole.pelser@gmail.com>
2024-12-05 20:31:22 +0000

    Refactor INFO command to improve message handling and user feedback

--- src/commands/INFO.cpp
+++ src/commands/INFO.cpp
@@ -2,19 +2,27 @@
 #include "General.hpp"
 #include "IRStream.hpp"
 #include "Server.hpp"
+#include "User.hpp"
 
 extern Server server;
 
 void INFO(IRStream &stream, string &args, User *user) {
 	(void)args;
+	string message;
 
 	stream.prefix().code(RPL_INFO).param(user->getNickname()).trail(server.getHostname() + " - Server").end();
 	stream.prefix().code(RPL_INFO).param(user->getNickname()).trail("Version 1.0").end();
 	stream.prefix().code(RPL_INFO).param(user->getNickname()).trail("Created by: Ole, Max & Mees").end();
-	stream.prefix()
-		.code(RPL_INFO)
-		.param(user->getNickname())
-		.trail("You are known as " + user->getRealname() + " have fun chatting 😃")
-		.end();
+	
+	message = (user->hasHandshake(H_REGISTERED))
+		? "You are known as " + user->getNickname() + " have fun chatting 😃"
+		: "You are not yet registered";
+	stream.prefix().code(RPL_INFO).param(user->getNickname()).trail(message).end();
+
+	message = (server.getUserCount() == "1")
+		? "There is currently 1 client connected"
+		: "There are currently " + server.getUserCount() +" clients connected";
+	stream.prefix().code(RPL_INFO).param(user->getNickname()).trail(message).end();
+
 	stream.prefix().code(RPL_ENDOFINFO).param(user->getNickname()).trail("End of /INFO list").end();
 }
