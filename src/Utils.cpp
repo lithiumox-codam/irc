e619625f5b5101574a82e0312f8db25c2d56a67a
olebol <ole.pelser@gmail.com>
2025-01-08 17:21:36 +0100

    Fix split functions to avoid pushing empty tokens into the result vector

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -14,6 +14,7 @@ using namespace std;
 vector<string> split(const string &str, const char &delim) {
 	vector<string> tokens;
 	string token;
+
 	for (const char &character : str) {
 		if (character == delim) {
 			tokens.push_back(token);
@@ -22,7 +23,11 @@ vector<string> split(const string &str, const char &delim) {
 			token += character;
 		}
 	}
-	tokens.push_back(token);
+
+	if (!token.empty()) {
+		tokens.push_back(token);
+	}
+
 	return tokens;
 }
 
@@ -47,20 +52,26 @@ vector<string> split(const string &str, const string &delim) {
 		}
 	}
 
-	tokens.push_back(token);
+	if (!token.empty()) {
+		tokens.push_back(token);
+	}
+
 	return tokens;
 }
 
 pair<string, string> splitPair(const string &str, const char &delim) {
 	size_t found = str.find(delim);
+
 	if (found == string::npos) {
 		return make_pair(str, "");
 	}
+
 	string first = str.substr(0, found);
 	string second = str.substr(found + 1);
 
 	if (!second.empty() && second[0] == ':') {
 		second = second.substr(1);
 	}
+
 	return make_pair(first, second);
 }
